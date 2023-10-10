016dd03e6d377d16c101f47cddc57f9843e17808
anzuv <127877298+anzuv@users.noreply.github.com>
2023-10-10 12:19:40 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useEffect,useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+const URL = "https://random-data-api.com/api/v2/appliances"
 function App() {
+
+const [brand, setBrand] = useState('')
+const [equipment, setEquipment] = useState('')
+
+useEffect(() => {
+  axios.get(URL)
+  .then((response)=>{
+  setBrand(response.data.brand)
+  setEquipment(response.data.equipment)
+  }).catch((error)=>{
+    alert(error)
+  })
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h3>Randomly selected appliance</h3>
+      <p>Brand:{brand}</p>
+      <p>Equipment:{equipment}</p>
+    
     </div>
   );
 }
